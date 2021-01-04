4e12f6474b21e437c10d3312253df8a96742ab9b
Christopher Joseph <alfonsoj93@hotmail.co.uk>
2021-01-04 18:27:09 +1100

    started to split out routes from main.py

--- main.py
+++ main.py
@@ -3,57 +3,7 @@ load_dotenv()
 
 from flask import Flask, request, jsonify, abort
 app = Flask(__name__)
-from database import cursor, connection 
 
-@app.route("/users", methods=["GET"])
-def users_index():
-    #Return all users
-    sql = "SELECT * FROM users"
-    cursor.execute(sql)
-    books = cursor.fetchall()
-    return jsonify(books)
 
-# @app.route("/books", methods=["POST"])
-# def book_create():
-#     #Create a new book
-#     sql = "INSERT INTO books (title) VALUES (%s);"
-#     cursor.execute(sql, (request.json["title"],))
-#     connection.commit()
-
-#     sql = "SELECT * FROM books ORDER BY ID DESC LIMIT 1"
-#     cursor.execute(sql)
-#     book = cursor.fetchone()
-#     return jsonify(book)
-
-# @app.route("/books/<int:id>", methods=["GET"])
-# def book_show(id):
-#     #Return a single book
-#     sql = "SELECT * FROM books WHERE id = %s;"
-#     cursor.execute(sql, (id,))
-#     book = cursor.fetchone()
-#     return jsonify(book)
-
-# @app.route("/books/<int:id>", methods=["PUT", "PATCH"])
-# def book_update(id):
-#     #Update a book
-#     sql = "UPDATE books SET title = %s WHERE id = %s;"
-#     cursor.execute(sql, (request.json["title"], id))
-#     connection.commit()
-
-#     sql = "SELECT * FROM books WHERE id = %s"
-#     cursor.execute(sql, (id,))
-#     book = cursor.fetchone()
-#     return jsonify(book)
-
-# @app.route("/books/<int:id>", methods=["DELETE"])
-# def book_delete(id):
-#     sql = "SELECT * FROM books WHERE id = %s;"
-#     cursor.execute(sql, (id,))
-#     book = cursor.fetchone()
-    
-#     if book:
-#         sql = "DELETE FROM books WHERE id = %s;"
-#         cursor.execute(sql, (id,))
-#         connection.commit()
-
-#     return jsonify(book)
+from users import users
+app.register_blueprint(users)
