0d19746bb608d5d82e65c18e5e02686cc83acd43
Christopher Joseph <alfonsoj93@hotmail.co.uk>
2021-01-05 20:28:02 +1100

    added marshmallow for serialisation and deserialisation

---
+++ schemas/Duo_fact_Schema.py
@@ -0,0 +1,10 @@
+
+from main import ma
+from models.Duo_facts import Duo_fact
+
+class Duo_fact_Schema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Duo_fact
+
+Duo_fact_schema = Duo_fact_Schema()
+Duo_facts_schema = Duo_fact_Schema(many=True)
